376d4dd41769ae622c736abbda56c77cbdbe9a0f
Michael Sumner <mdsumner@gmail.com>
2016-11-23 23:02:47 +1100

    chlorophyll-a data update, workflow

--- inst/working/workflow/bulk_aceeco.r
+++ inst/working/workflow/bulk_aceeco.r
@@ -36,12 +36,29 @@ read_i_mag <- function(i, files) {
   raadtools:::.rotate(vlen(udat, vdat))
 }
 
+read_i_chl <- function(i, files) {
+  ex <- extent(-180, 180, -80, -30)
+  crop(raster(files$fullname[i], varname = "chlor_a"), ex)
+}
 library(aceecostats)
 library(raadtools)
 outf <- "/mnt/acebulk"
 ice <- build_bulk_file(icefiles(), file.path(outf, "ice.grd"), read_i_ice, layer_prefix = "ice")
 sst <- build_bulk_file(sstfiles(), file.path(outf, "sst.grd"), read_i_sst, layer_prefix = "sst")
 mag <- build_bulk_file(currentsfiles(), file.path(outf, "mag.grd"), read_i_mag, layer_prefix = "mag")
+
+outf <- "/home/shared/data/assessment/acebulk/summaries"
+library(dplyr)
+library(tibble)
+sw <- ocfiles(time.resolution = "monthly", product = "SeaWiFS", varname = "CHL", type = "L3m", ext = "nc")
+md <- ocfiles(time.resolution = "monthly", product = "MODISA", varname = "CHL", type = "L3m", ext = "nc")
+oc <- bind_rows(sw %>% filter(date < min(md$date)), md) %>%   as_tibble()
+vr <- ocfiles(time.resolution = "monthly", product = "VIIRS", varname = "SNPP_CHL", type = "L3m", ext = "nc")
+oc <- bind_rows(oc %>% filter(date < min(vr$date)), vr)
+
+system.time({
+chl <- build_bulk_file(oc, file.path(outf, "chl.grd"), read_i_chl, layer_prefix = "month_chl")
+})
 
 
 ## run each summ script, and then
