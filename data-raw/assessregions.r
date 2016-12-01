d1dcb3f92e3cae3cd80a64c0e9cca0642d684f2e
Michael Sumner <mdsumner@gmail.com>
2016-12-01 01:04:48 +0000

    aes_region_simple

--- data-raw/assessregions.r
+++ data-raw/assessregions.r
@@ -17,3 +17,35 @@ aes_region_ll$ID <- 1:nrow(aes_region_ll)
 devtools::use_data(aes_region, compress = "xz")
 devtools::use_data(aes_region_ll, compress = "xz")
 
+
+library(aceecostats)
+library(dplyr)
+library(rgeos)
+library(sp)
+
+aes_region$index <- seq(nrow(aes_region))
+aes_region$Shelf <- ifelse(aes_region$BathyClass == "Continent", "Shelf", "Ocean")
+
+
+p1 <- gUnionCascaded(aes_region, paste(aes_region$SectorName, aes_region$Zone, aes_region$Shelf))
+
+library(dplyr)
+## gets a bit shaky, this might get reordered somewhere
+d <- aes_region@data %>% group_by(SectorName, Zone, Shelf) %>% summarize(area_km2 = sum(area_km2))
+
+aes_region_simple <- SpatialPolygonsDataFrame(p1, as.data.frame(d), match.ID = FALSE)
+
+sector_colour <- function(secname) {
+  setNames(c("#7CAE00", "#00BFC4","#C77CFF", "#F8766D"), 
+           c("Atlantic","Indian", "EastPacific","WestPacific"))[secname]
+  
+}
+
+aes_region_simple$colour <- sector_colour(aes_region_simple$SectorName)
+devtools::use_data(aes_region_simple, compress = "xz")
+## CHECK!  
+#plot(aes_region_simple, col = aes_region_simple$colour)
+#text(coordinates(aes_region_simple), lab = sprintf("%s \n %s \n %s", aes_region_simple$SectorName, aes_region_simple$Zone, aes_region_simple$Shelf))
+
+
+
