67b88beafd8e439ac4048c84b0a1556d71224e91
Michael Sumner <mdsumner@gmail.com>
2016-12-20 14:31:01 +0000

    package stuff

--- R/worker-plot.r
+++ R/worker-plot.r
@@ -1,10 +1,26 @@
 
-## worker plot functions
-## date range for the sparkline
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
 mk_sparkline_range <- function() ISOdatetime(c(1980, 2016), c(1, 11), 1, 0, 0, 0, tz = "GMT")
 
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
 dummyplot <- function() plot(1, 1, type = "p", axes = FALSE, xlab = "", ylab = "")
 
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
 layout_m <- function() {
   tx <- textConnection(
     "1,1,1,1,1,1,1,1,3,3,3,3,3,3,3,3
@@ -49,6 +65,17 @@ layout_m <- function() {
     6,6,6,6,6,6,6,6,8,8,8,8,8,8,8,8")
   as.matrix(read.csv(tx, header=F))
 }
+
+
+#' Title
+#'
+#' @param x 
+#' @param y 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 textheadtail <- function(x, y) {
   xx <- c(head(x, 1), tail(x, 1))
   yy <- c(head(y, 1), tail(y, 1))
@@ -60,11 +87,28 @@ path2seg <- function(x) {
   ## this is a trick of array logic to generate paired indexes from a sequence
   head(suppressWarnings(matrix(x, nrow = length(x) + 1, ncol = 2, byrow = FALSE)), -2L)
 }
+#' Title
+#'
+#' @param x 
+#' @param col 
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 segmentlines <- function(x, col, ...) {
   ind <- path2seg(seq(nrow(x)))
   segments(x[ind[,1], 1], x[ind[,1], 2], x[ind[,2], 1], x[ind[,2], 2], 
            col = col, ...)
 }
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
 sector_colour <- function(secname) {
   setNames(c("#7CAE00", "#00BFC4","#C77CFF", "#F8766D"), 
            c("Atlantic","Indian", "EastPacific","WestPacific"))[secname]
@@ -91,6 +135,21 @@ decselect <- function(n) {
   #c("1980-1992", "1991-2004","2002-2016")[n]
   c("1981-1990", "1990-1999","1999-2008", "2008-2016")[n]
 }
+
+#' Worker function for histogram/density
+#'
+#' @param v measured values (e.g. ice season duration)
+#' @param w weight values (e.g. area of this cell)
+#'
+#' @export
+do_hist <- function(v, w = NULL) {
+  the.his <- hist(v, breaks=50, plot = FALSE)
+  multiplier <- (the.his$counts / the.his$density)[1]
+  the.den <- density(v, from=min(v), to=max(v), weights = w)
+  data.frame(x=the.den$x, y=the.den$y*multiplier)
+}
+
+
 do_density <- function(v, w = NULL) {
   the.his <- hist(v, breaks=50, plot = FALSE)
   multiplier <- (the.his$counts / the.his$density)[1]
