cfd25ffbd6be5bfc94a247223e0a0ae0a851f25f
Michael Sumner <Michael.Sumner@aad.gov.au>
2017-01-19 15:36:18 +1100

    update GeoJSON

--- data-raw/geojson.r
+++ data-raw/geojson.r
@@ -1,7 +1,7 @@
 library(aceecostats)
 library(geojson)
 
-x <- aes_region_ll
+x <- aes_zone_ll
 
 
 r0 <- col2rgb(x$colour, alpha = TRUE)
@@ -9,3 +9,13 @@ x$fill <- rgb(r0[1, ], r0[2, ], r0[3, ], max = 255)
 x$`fill-opacity` <- r0[4, ]/255
 x$colour <- NULL
 geojson::geo_write(as.geojson(x), "data-raw/aes_region_ll.geojson")
+
+
+x <- aes_zone
+
+
+r0 <- col2rgb(x$colour, alpha = TRUE)
+x$fill <- rgb(r0[1, ], r0[2, ], r0[3, ], max = 255)
+x$`fill-opacity` <- r0[4, ]/255
+x$colour <- NULL
+geojson::geo_write(as.geojson(x), "data-raw/aes_region.geojson")
