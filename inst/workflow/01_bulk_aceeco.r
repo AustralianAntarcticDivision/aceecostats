ebf3daf4ab01bdc401520407c2f5c6dbda51f66b
Michael Sumner <mdsumner@gmail.com>
2017-08-08 11:55:46 +0000

    update ice and sst (using tidync)

--- inst/workflow/01_bulk_aceeco.r
+++ inst/workflow/01_bulk_aceeco.r
@@ -24,6 +24,28 @@ devtools::use_data(aes_icefiles, aes_sstfiles, overwrite = TRUE)
 ## takes about 600 seconds
 ice <- build_bulk_file(aes_icefiles, file.path(dp, "ice.grd"), read_i_ice, layer_prefix = "ice")
 
+
+library(dplyr)
+library(tidync)
+tnc <- tidync(files$fullname[i]) 
+hf <- tnc %>% 
+  hyper_filter(lat  = between(lat, -80, -30))
+update_source <- function(x, source) {
+  src <- attr(x, "source")
+  src$source <- source
+  attr(x, "source") <- src
+  x
+}
+read_i_sst_tidync <- function(i, files) {
+  arr <- update_source(hf, files$fullname[i]) %>% 
+    hyper_slice(select_var = c("sst", "ice"))
+  sst <- arr$sst
+  sst[!is.na(arr$ice)] <- NA_real_
+  sst2 <- rbind(sst[721:1440, ], sst[1:720, ])
+  setExtent(raster(t(sst2[, ncol(sst2):1])), extent(-180, 180, -80, -30))
+}
+
+
 ## takes about 4856 seconds
-sst <- build_bulk_file(aes_sstfiles, file.path(dp, "sst.grd"), read_i_sst, layer_prefix = "sst")
+sst <- build_bulk_file(aes_sstfiles, file.path(dp, "sst.grd"), read_i_sst_tidync, layer_prefix = "sst")
 
