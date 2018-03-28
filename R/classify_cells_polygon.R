da56ea1048663b764d63ce817b5fad5611e20fe8
Michael Sumner <mdsumner@gmail.com>
2018-03-28 02:26:18 +0000

    add example

--- R/classify_cells_polygon.R
+++ R/classify_cells_polygon.R
@@ -7,6 +7,28 @@
 #' @param grid raster for the cells
 #' @param polygons SpatialPolygonsDataFrame
 #' @export
+#' @examples 
+#' library(aceecostats)
+#'  library(raster)
+#'  library(dplyr)
+#'  library(ggplot2)
+#'  
+#'  dp <- "/home/acebulk/data"
+#'  db <- dplyr::src_sqlite(file.path(dp, "habitat_assessment.sqlite3"))
+#'  
+#'  dens <- tbl(db, "ice_days_density_tab") %>% dplyr::filter(Zone == "Continent")
+#'  d <- dens %>% dplyr::collect(n = Inf)
+#'  icegrid <- raster(extent(-3950000, 3950000, -3950000, 4350000), 
+#'                    crs = "+proj=stere +lat_0=-90 +lat_ts=-70 +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378273 +b=6356889.449 +units=m +no_defs", 
+#'                    res = 25000)
+#'  
+#'  ccamlr <- readRDS("/home/shared/data/assessment/sectors/ccamlr_statareas.rds")
+#'  ccamlr48 <- subset(ccamlr, grepl("48", name))
+#'  d$ccamlr <- classify_cells_by_polygon(d$cell_, icegrid, ccamlr48[, "name"])
+#'  
+#'  
+#'  d %>% dplyr::filter(!is.na(ccamlr)) %>%  mutate(x = xFromCell(icegrid, cell_), y = yFromCell(icegrid, cell_)) %>% 
+#'    ggplot(aes(x, y, fill = ccamlr)) + geom_raster() + ggplot2::coord_equal()
 classify_cells_by_polygon <- function(cells, grid, polygons) {
   p_polygons <- spTransform(polygons, projection(grid))
   p_polygons$idx <- seq_len(nrow(p_polygons))
