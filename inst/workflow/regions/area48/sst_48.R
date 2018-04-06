f62873fdcd8f3c7a2ac6e41fa4f9ede714f4020c
Michael Sumner <mdsumner@gmail.com>
2018-04-06 08:50:13 +0000

    sst for region 48

--- inst/workflow/regions/area48/sst_48.R
+++ inst/workflow/regions/area48/sst_48.R
@@ -3,26 +3,30 @@ library(raster)
 library(dplyr)
 library(ggplot2)
 
-library(tidyr)
-library(DT)
 #db <- src_sqlite("/mnt/acebulk/habitat_assessment_output.sqlite3")
 dp <- "/home/acebulk/data"
 db <- dplyr::src_sqlite(file.path(dp, "habitat_assessment.sqlite3"))
 
 dens <- tbl(db, "sst_density_tab") %>% 
-  dplyr::filter(Zone == "High-Latitude")
+  dplyr::filter(SectorName != "Indian") %>% 
+  dplyr::filter(SectorName != "WestPacific")
+
 d <- dens %>% dplyr::collect(n = Inf)
-#d <- as.data.frame(roc::bin2lonlat(dens$bin_num, 4320))
 
 sstgrid <- raster(extent(-180, 180, -80, -30), crs = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0", res = 0.25)
-## get depth
-topo <- readAll(raadtools::readtopo("ibcso"))
-uxy <- distinct(d, cell_) %>% dplyr::mutate(x = xFromCell(sstgrid, cell_), y = yFromCell(sstgrid, cell_))
-uxy$depth <- raster::extract(topo, 
-                             rgdal::project(
-                               as.matrix(uxy[c("x", "y")]), 
-                               projection(topo)))
-d <- d %>% dplyr::inner_join(uxy[c("cell_", "depth")])
+
+
+ccamlr <- readRDS("/home/shared/data/assessment/sectors/ccamlr_statareas.rds")
+ccamlr48 <- subset(ccamlr, grepl("48", name))
+rm(ccamlr)
+## use dopey indexing because assuming first column
+d$ccamlr <- classify_cells_by_polygon(d$cell_, sstgrid, ccamlr48[, "name"])
+
+d <- d %>% dplyr::filter(!is.na(ccamlr))
 op <- options(warn = -1)
-ggplot(d %>% dplyr::filter(depth >= -500),  aes(x = max, group = decade, weight = 25, colour = decade)) + 
-  geom_density() + facet_wrap(SectorName ~ season) 
+ggplot(d %>% dplyr::filter(SectorName ==  "Atlantic"),  
+       aes(x = max, group = decade, weight = area)) + 
+  geom_density() + facet_wrap(Zone ~ season)
+
+options(op)
+
