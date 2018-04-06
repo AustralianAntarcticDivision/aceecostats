7997ed4bee50b5c348fc7931b995da2d5e5c1686
Michael Sumner <mdsumner@gmail.com>
2018-04-06 04:31:10 +0000

    chl + ice CCAMLR 48

---
+++ inst/workflow/regions/area48/chlorophyll_48.R
@@ -0,0 +1,41 @@
+
+## preparation
+library(aceecostats)
+library(raster)
+library(dplyr)
+library(ggplot2)
+
+library(tidyr)
+library(DT)
+#db <- src_sqlite("/mnt/acebulk/habitat_assessment_output.sqlite3")
+dp <- "/home/acebulk/data"
+db <- dplyr::src_sqlite(file.path(dp, "habitat_assessment.sqlite3"))
+
+dens <- tbl(db, "chl_25k_tab")  %>% 
+  dplyr::filter(SectorName != "Indian") %>% 
+  dplyr::filter(SectorName != "WestPacific")
+
+d <- dens %>% dplyr::collect(n = Inf)
+
+prjj <-         "+proj=laea +lat_0=-90 +datum=WGS84"
+chlgrid <- raster(spex::buffer_extent(projectExtent(raster(extent(-180, 180, -90, -30), 
+                                                         crs = "+init=epsg:4326"), 
+                                                  prjj), 25000), 
+                res = 25000, crs = prjj)
+
+ccamlr <- readRDS("/home/shared/data/assessment/sectors/ccamlr_statareas.rds")
+ccamlr48 <- subset(ccamlr, grepl("48", name))
+## use dopey indexing because assuming first column
+d$ccamlr <- classify_cells_by_polygon(d$cell25, chlgrid, ccamlr48[, "name"])
+
+
+op <- options(warn = -1)
+ggplot(d %>% dplyr::filter(SectorName == "Atlantic"),  aes(x = chla_johnson, group = decade, weight = 25, colour = decade)) + 
+  geom_density() + facet_wrap(SectorName ~ season) + scale_x_log10()
+ggsave("inst/workflow/regions/area48/chl_48_Atlantic.png")
+ggplot(d %>% dplyr::filter(SectorName == "EastPacific"),  aes(x = chla_johnson, group = decade, weight = 25, colour = decade)) + 
+  geom_density() + facet_wrap(SectorName ~ season) + scale_x_log10()
+ggsave("inst/workflow/regions/area48/chl_48_EastPacific.png")
+
+options(op)
+
--- inst/workflow/regions/area48/ice_48.R
+++ inst/workflow/regions/area48/ice_48.R
@@ -9,8 +9,9 @@ library(ggplot2)
 dp <- "/home/acebulk/data"
 db <- dplyr::src_sqlite(file.path(dp, "habitat_assessment.sqlite3"))
 
-dens <- tbl(db, "ice_days_density_tab") %>% dplyr::filter(Zone == "Continent")
+dens <- tbl(db, "ice_days_density_tab") 
 d <- dens %>% dplyr::collect(n = Inf)
+
 icegrid <- raster(extent(-3950000, 3950000, -3950000, 4350000), 
                   crs = "+proj=stere +lat_0=-90 +lat_ts=-70 +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378273 +b=6356889.449 +units=m +no_defs", 
                   res = 25000)
@@ -18,10 +19,23 @@ icegrid <- raster(extent(-3950000, 3950000, -3950000, 4350000),
 
 ccamlr <- readRDS("/home/shared/data/assessment/sectors/ccamlr_statareas.rds")
 ccamlr48 <- subset(ccamlr, grepl("48", name))
+                                                        ## use dopey indexing because assuming first column
 d$ccamlr <- classify_cells_by_polygon(d$cell_, icegrid, ccamlr48[, "name"])
+
+
+## remove cells outside 48
+d <- d %>% dplyr::filter(!is.na(ccamlr))
 op <- options(warn = -1)
 ggplot(d  %>% 
-         dplyr::filter(days < 365 ), aes(x = days, group = decade, weight = area, colour = decade)) + geom_density() + facet_wrap(SectorName ~ season)
-ggsave("inst/workflow/regions/area48/ice_48.png")
+         dplyr::filter(days > 0 & days < 365, SectorName == "Atlantic"), 
+       aes(x = days, group = decade, weight = area, colour = decade)) + 
+  geom_density() + facet_wrap(~Zone, nrow = 3)
+ggsave("inst/workflow/regions/area48/ice_48_Atlantic.png")
+ggplot(d  %>% 
+         dplyr::filter(days > 0 & days < 365, SectorName == "EastPacific"), 
+       aes(x = days, group = decade, weight = area, colour = decade)) + 
+  geom_density() + facet_wrap(~Zone, nrow = 3)
+ggsave("inst/workflow/regions/area48/ice_48_EastPacific.png")
+
 par(op)
 
