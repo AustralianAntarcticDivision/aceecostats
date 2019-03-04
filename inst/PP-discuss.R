a54f7cdcf63683f04e2719fd6da2c67b7d652b2f
Michael Sumner <mdsumner@gmail.com>
2019-03-04 11:27:07 +1100

    discuss

---
+++ inst/PP-discuss.R
@@ -0,0 +1,50 @@
+# Tony Worby paper from 1990s about iceberg ejection into ACC
+
+# l2gen sea ice parameterization
+
+## AC:
+
+# cloud, AC interested in
+
+# standing stock now, in a month's time, how much consumed
+
+# accumulated poc, old code with AC
+
+# Arrigo did PP, are they good estimates?
+
+## RJ: 
+
+# Arrigo great only in Ross Sea, no good in frontal zones
+# (because diff. amount of chl-a, it's not less efficient)
+#  photosynthetic efficiency (Pvopt, photsyn rate - assumes more efficient with higher temp)
+# but this assumption falls down as you go south, life history of organisms is ignored
+
+# iron is crucial, if there's not enough the factory cannot scale up
+#  so result is a lower efficiency 
+
+# Behrenfield used everywhere else, 
+# take improved chla, plug into the Vgpm and see what difference is
+# - will change the magnitudes, but also give more dynamic range
+# - OSU Oregon state, Behrenfield primary productivity
+# - nutrients from climatology
+# - is Behrenfield seasonal or?
+# - summer PP is chl-a, euphotic zone depth, some measure of photosyn efficiency Pvop, some measure of
+#  daylenght (light availability) - long days, but low light is very important, but too cold or low nutrients kills
+
+# http://www.science.oregonstate.edu/ocean.productivity/cbpm2.code.php
+
+# use euphotic zone as 1/KD490 as 1 optical depth
+# read paper Behrenfeld consumer's guide 1997
+
+
+
+
+
+
+
+
+
+
+
+
+
